984c6f01998b570ec757ebdbf7da9cf8c96dbf73
hackycoder0951 <jignesh.visio.17@gmail.com>
2025-07-07 00:36:34 +0530

    Add ResetPassword route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Index from "./pages/Index";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
 import Feed from "./pages/Feed";
 import Profile from "./pages/Profile";
 import QandA from "./pages/QandA";
@@ -39,6 +40,7 @@ const App = () => (
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="/reset-password" element={<ResetPassword />} />
               <Route path="/app" element={
                 <ProtectedRoute>
                   <Layout />
